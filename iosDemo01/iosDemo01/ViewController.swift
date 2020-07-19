b4bd6c201c4ed5081fb9c333591b7a28ca611bdf
Thomas Tsui <thomas.1011321@gmail.com>
2020-07-19 23:51:57 +0800

    Project init

---
+++ iosDemo01/iosDemo01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosDemo01
+//
+//  Created by Thomas Tsui on 19/7/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
