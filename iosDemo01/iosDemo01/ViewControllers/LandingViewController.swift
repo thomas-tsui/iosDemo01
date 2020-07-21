f28a8b43d10db585f4690e6e020d1af30a925888
Thomas Tsui <thomas.1011321@gmail.com>
2020-07-21 16:08:32 +0800

    edited Landing page UI and added dummy data for view display

--- iosDemo01/iosDemo01/ViewControllers/LandingViewController.swift
+++ iosDemo01/iosDemo01/ViewControllers/LandingViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class LandingViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
+    private let recommendHeaderViewIdentifier: String = "recommendHeaderViewIdentifier"
+    private let topFreeAppCellIdentifier: String = "topFreeAppCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initTableView()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    private func initTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TopFreeAppCell.fromNib(), forCellReuseIdentifier: topFreeAppCellIdentifier)
+    }
 }
 
+extension LandingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 204
+    }
+}
+
+extension LandingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let topFreeAppCell = tableView.dequeueReusableCell(withIdentifier: topFreeAppCellIdentifier, for: indexPath) as? TopFreeAppCell else {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Test Cell: \(indexPath.row)"
+            return cell
+        }
+        topFreeAppCell.setIndexText("\(indexPath.row+1)")
+        topFreeAppCell.setImage(#imageLiteral(resourceName: "demoIcon1"))
+        topFreeAppCell.setImageCornerRadius(indexPath.row%2==1)
+        topFreeAppCell.setAppNameLabelText("APP APP APP")
+        topFreeAppCell.setAppTypeLabelText("Game")
+        topFreeAppCell.setRatingNumCountLabelText("17")
+        return topFreeAppCell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = GrossingAppView.fromNib()
+        header.setHeaderCellTitle("推介")
+        for index in 0..<10 {
+            header.addItem(appTitle: "APP APP APP APP APP", appTypeTitle: "Game", id: index)
+        }
+        header.reload()
+        return header
+    }
+}
