5a20d8d4e90d6498c3f278acfd7d9ce87c14ad30
Thomas Tsui <thomas.1011321@gmail.com>
2020-07-22 17:32:18 +0800

    added landing view model with 1. Grossing App API 2. Top Free App API 3. App Look Up API and api calling functions for landingVC

---
+++ iosDemo01/iosDemo01/ViewModels/LandingViewModel.swift
@@ -0,0 +1,96 @@
+//
+//  LandingViewModel.swift
+//  iosDemo01
+//
+//  Created by Thomas Tsui on 22/7/2020.
+//
+
+import UIKit
+import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
+
+protocol LandingViewControllerDelegate: NSObjectProtocol {
+    func reloadTable()
+}
+
+class LandingViewModel: NSObject {
+    var grossingAppArray: [TopFreeAppDetails] = []
+    var appDetailsArray: [TopFreeAppDetails] = []
+    
+    var currentTopAppDataCount: Int = 10
+    
+    weak var delegate: LandingViewControllerDelegate?
+    
+    func getTopGrossingAppList(appCount:Int = 10) {
+        let URL = "https://itunes.apple.com/hk/rss/topgrossingapplications/limit=\(appCount)/json"
+        Alamofire.request(URL).responseObject(keyPath: "feed") { (response: DataResponse<TopFreeAppListResponse>) in
+            switch response.result {
+            case .success:
+                guard let _response = response.result.value, let entry = _response.entry else {
+                    print("Error")
+                    return
+                }
+                self.grossingAppArray = entry
+                self.delegate?.reloadTable()
+            case .failure:
+                print("Error")
+            }
+        }
+    }
+    
+    func getTopFreeAppList(appCount:Int = 10) {
+        let appListURL = "https://itunes.apple.com/hk/rss/topfreeapplications/limit=\(appCount)/json"
+        Alamofire.request(appListURL).responseObject(keyPath: "feed") { (response: DataResponse<TopFreeAppListResponse>) in
+            switch response.result {
+            case .success:
+                guard let _response = response.result.value, let entry = _response.entry else {
+                    print("Error")
+                    return
+                }
+                self.appDetailsArray = entry
+                if entry.count > 0 {
+                    let combinedIdString = self.getAppsId(array: entry)
+                    let appsDetailURL = "https://itunes.apple.com/hk/lookup?id=\(combinedIdString)"
+                    Alamofire.request(appsDetailURL).responseObject { (response: DataResponse<AppDetailsResponse>) in
+                        switch response.result {
+                        case .success:
+                            guard let _response = response.result.value, let results = _response.results else {
+                                print("Error")
+                                return
+                            }
+                            print(results)
+                            for (index, value) in results.enumerated() {
+                                self.appDetailsArray[index].averageUserRatingForCurrentVersion = value.averageUserRatingForCurrentVersion
+                                self.appDetailsArray[index].userRatingCountForCurrentVersion = value.userRatingCountForCurrentVersion
+                            }
+                            self.delegate?.reloadTable()
+                        case .failure:
+                            print("Error")
+                        }
+                    }
+                }
+                
+                self.delegate?.reloadTable()
+            case .failure:
+                print("Error")
+            }
+        }
+    }
+    
+    func getAppsId(array: [TopFreeAppDetails]) -> String {
+        var combinedIdString = ""
+        for item in array {
+            guard let _id = item.id else { continue }
+            combinedIdString += _id + ","
+        }
+        return combinedIdString
+    }
+}
+
+extension Double {
+    func round(to places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
